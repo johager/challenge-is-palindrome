52cc61b8f76269a4c621ff13d66c8a9257816017
James Hager <james@avantiapplications.com>
2022-01-26 13:39:51 -0700

    Initial commit

---
+++ isPalindrome.js
@@ -0,0 +1,8 @@
+/*
+Return true if this word is a palindrome. false if it is not. A palindrome is a word that is spelled the same backwards and forwards.
+
+Treat spaces and uppercase letters normally—so “Racecar” wouldn’t be a palindrome since “R” and “r” aren’t the same letter.
+*/
+
+// Write your code below
+
